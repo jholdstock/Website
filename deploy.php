0dd7ddb2698262e6c9facdbcbc11ad491f3b81c1
jholdstock <jam.jar69@gmail.com>
2014-02-23 14:45:21 +0000

    w

--- deploy.php
+++ deploy.php
@@ -1,27 +1,30 @@
 <?php
 
-$logheader = "====================".PHP_EOL."New Request was made".PHP_EOL."====================".PHP_EOL.PHP_EOL;
+$logLocation = "./logs/github.txt";
+$scriptLocation = "/var/www/deploy.sh"
+
 try
 {
+	$logheader = "====================".PHP_EOL."New Request was made".PHP_EOL."====================".PHP_EOL.PHP_EOL;
+
 	$payload = $_REQUEST['payload'];
-	var_dump($payload); die;
 	$payload = stripcslashes($payload);
 	$payload = json_decode($payload);
 
 	//log the request
-	file_put_contents('./logs/github.txt', $logheader, FILE_APPEND);
-	file_put_contents('./logs/github.txt', print_r($payload, true), FILE_APPEND);
-	file_put_contents('./logs/github.txt', PHP_EOL, FILE_APPEND);
+	file_put_contents($logLocation, $logheader, FILE_APPEND);
+	file_put_contents($logLocation, print_r($payload, true), FILE_APPEND);
+	file_put_contents($logLocation, PHP_EOL, FILE_APPEND);
 
 	if ($payload->ref === 'refs/heads/master')
 	{
-		$deploy = shell_exec("/var/www/deploy.sh");
+		$deploy = shell_exec($scriptLocation);
 
-		file_put_contents('./logs/github.txt', $deploy.PHP_EOL, FILE_APPEND);
+		file_put_contents($logLocation, $deploy.PHP_EOL, FILE_APPEND);
 	}
 }
 catch(Exception $e) 
 {
-	file_put_contents('./logs/github.txt', $e, FILE_APPEND);
+	file_put_contents($logLocation, $e, FILE_APPEND);
 	exit(0);
 }
