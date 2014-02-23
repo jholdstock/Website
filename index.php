11cc896401166e4355963303352d72e07ae6edac
jholdstock <jam.jar69@gmail.com>
2014-02-23 13:12:34 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Jamie Holdstock</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="main.css" />
+	</head>
+
+	<body>
+		<div id="window">
+			<h1>Welcome</h1>
+			<p>This is my first page</p>
+		</div>
+	</body>
+
+</html>
