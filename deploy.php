6ba535b227268a9780c43b46bfa9bae927dd84e7
jholdstock <jam.jar69@gmail.com>
2014-02-23 15:19:37 +0000

    egf2

--- deploy.php
+++ deploy.php
@@ -1,19 +1,3 @@
 <?php
 
-$scriptLocation = "/var/www/deploy.sh";
-
-try
-{
-	$payload = $_REQUEST['payload'];
-	$payload = stripcslashes($payload);
-	$payload = json_decode($payload);
-
-	if ($payload->ref === 'refs/heads/master')
-	{
-		$deploy = shell_exec($scriptLocation);
-	}
-}
-catch(Exception $e) 
-{
-	exit(0);
-}
+shell_exec("/var/www/deploy.sh");
