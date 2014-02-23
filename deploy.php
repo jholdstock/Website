d983d904ce02e20b34703a890f84948d99e5bca0
jholdstock <jam.jar69@gmail.com>
2014-02-23 16:04:35 +0000

    egf2

--- deploy.php
+++ deploy.php
@@ -1,8 +1,7 @@
 <?php
 
-shell_exec("/scripts/deploy.sh");
 
 echo "start";
-$output = shell_exec("/var/www/deploy.sh");
+$output = shell_exec("/scripts/deploy.sh");
 echo "<pre>$output</pre>";
 echo "end";
