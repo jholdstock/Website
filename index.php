14c0b4142da2df9dfd8a587de9cca9126bf5b84a
jholdstock <jam.jar69@gmail.com>
2014-02-25 13:27:35 +0000

    First real commit lol

--- index.php
+++ index.php
@@ -5,13 +5,29 @@
 		<title>Jamie Holdstock</title>
 		<meta charset="UTF-8" />
 		<link rel="stylesheet" type="text/css" href="main.css" />
+		<script src="jquery-1.11.0.min.js"></script>
+		<script src="main.js"></script>
+		
+		<script>
+		</script>
+
 	</head>
 
 	<body>
-		<div id="window">
-			<h1>Welcome</h1>
-			<p>This is my first page</p>
+
+		<div id="menu">	
+			<ul>
+				<li><a href="about.php" class="current">About</a></li><li><a href="projects.php">Projects</a></li><li><a href="about.php">CV</a></li><li><a href="projects.php">Contact</a></li>
+			</ul>
 		</div>
+
+		<div id="window">
+		</div>
+
+		<div id="copyright">
+			&copy; <?php echo date("Y"); ?> Jamie Holdstock
+		</div>
+
 	</body>
 
 </html>
