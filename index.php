a3fec0f75dd382a404fc3c57de366ca1e6226778
jholdstock <jam.jar69@gmail.com>
2014-02-25 15:22:50 +0000

    added normalize css and favicon

--- index.php
+++ index.php
@@ -2,11 +2,21 @@
 <html>
 
 	<head>
-		<title>Jamie Holdstock</title>
 		<meta charset="UTF-8" />
-		<link rel="stylesheet" type="text/css" href="main.css" />
-		<script src="jquery-1.11.0.min.js"></script>
-		<script src="main.js"></script>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		
+		<title>Jamie Holdstock</title>
+
+		<link rel="stylesheet" type="text/css" href="css/vendor/main.css" />
+		<link rel="stylesheet" type="text/css" href="css/vendor/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/custom.css" />
+
+		<link rel="shortcut icon" href="img/favicon.ico" />
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+		<script src="js/main.js"></script>
 		
 		<script>
 		</script>
@@ -17,7 +27,7 @@
 
 		<div id="menu">	
 			<ul>
-				<li><a href="about.php" class="current">About</a></li><li><a href="projects.php">Projects</a></li><li><a href="about.php">CV</a></li><li><a href="projects.php">Contact</a></li>
+				<li><a href="about.php" class="current">About</a></li><li><a href="projects.php">Projects</a></li><li><a href="cv.php">CV</a></li><li><a href="contact.php">Contact</a></li>
 			</ul>
 		</div>
 
