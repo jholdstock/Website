ecaac859c28f321555d8686226bb59faa1fa8096
jholdstock <jam.jar69@gmail.com>
2014-02-25 16:07:42 +0000

    added google analytics

--- index.php
+++ index.php
@@ -2,22 +2,33 @@
 <html>
 
 	<head>
+		<title>Jamie Holdstock</title>
+		
+		<!-- Meta data -->
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		
-		<title>Jamie Holdstock</title>
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
 
+		<!-- Style -->
 		<link rel="stylesheet" type="text/css" href="css/vendor/main.css" />
 		<link rel="stylesheet" type="text/css" href="css/vendor/normalize.css" />
 		<link rel="stylesheet" type="text/css" href="css/custom.css" />
-
 		<link rel="shortcut icon" href="img/favicon.ico" />
 		
+		<!-- Scripts -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-
 		<script src="js/main.js"></script>
 		
+        <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-48413007-1', 'jamieholdstock.com'); ga('send', 'pageview');
+		</script>
+
 		<script>
 		</script>
 
